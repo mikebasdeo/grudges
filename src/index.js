b753b0571606700bfa9eb6ec8ec35a4c27e09bfe
Mike Basdeo <mikebasdeo@gmail.com>
2020-10-14 16:01:39 -0400

    boom roasted.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
 
+
+import Amplify from '@aws-amplify/core'
+import { Auth } from '@aws-amplify/auth'
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
+Auth.configure(awsconfig)
+
+
 ReactDOM.render(<Application />, document.getElementById('root'));
