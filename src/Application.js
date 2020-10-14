e9a24a91c2c7009736bbcb4ddb5bd45800d2400b
Mike Basdeo <mikebasdeo@gmail.com>
2020-10-14 15:00:04 -0400

    Revert "maybe auth?"
    This reverts commit 8f661638ed6426241f7fbfa2993c8bb8ff22c837.

--- src/Application.js
+++ src/Application.js
@@ -1,57 +1,54 @@
-import React, { Component } from 'react'
-import NewGrudge from './NewGrudge'
-import Grudges from './Grudges'
-import './Application.css'
-
-// import { withAuthenticator } from '@aws-amplify/ui-react'
-import { withAuthenticator } from "aws-amplify-react";
+import React, { Component } from 'react';
+import NewGrudge from './NewGrudge';
+import Grudges from './Grudges';
+import './Application.css';
 
 class Application extends Component {
   state = {
     grudges: [],
-  }
+  };
 
-  addGrudge = (grudge) => {
-    this.setState({ grudges: [grudge, ...this.state.grudges] })
-  }
+  addGrudge = grudge => {
+    this.setState({ grudges: [grudge, ...this.state.grudges] });
+  };
 
-  removeGrudge = (grudge) => {
+  removeGrudge = grudge => {
     this.setState({
-      grudges: this.state.grudges.filter((other) => other.id !== grudge.id),
-    })
-  }
+      grudges: this.state.grudges.filter(other => other.id !== grudge.id),
+    });
+  };
 
-  toggle = (grudge) => {
+  toggle = grudge => {
     const othergrudges = this.state.grudges.filter(
-      (other) => other.id !== grudge.id
-    )
-    const updatedGrudge = { ...grudge, avenged: !grudge.avenged }
-    this.setState({ grudges: [updatedGrudge, ...othergrudges] })
-  }
+      other => other.id !== grudge.id,
+    );
+    const updatedGrudge = { ...grudge, avenged: !grudge.avenged };
+    this.setState({ grudges: [updatedGrudge, ...othergrudges] });
+  };
 
   render() {
-    const { grudges } = this.state
-    const unavengedgrudges = grudges.filter((grudge) => !grudge.avenged)
-    const avengedgrudges = grudges.filter((grudge) => grudge.avenged)
+    const { grudges } = this.state;
+    const unavengedgrudges = grudges.filter(grudge => !grudge.avenged);
+    const avengedgrudges = grudges.filter(grudge => grudge.avenged);
 
     return (
-      <div className='Application'>
+      <div className="Application">
         <NewGrudge onSubmit={this.addGrudge} />
         <Grudges
-          title='Unavenged Grudges'
+          title="Unavenged Grudges"
           grudges={unavengedgrudges}
           onCheckOff={this.toggle}
           onRemove={this.removeGrudge}
         />
         <Grudges
-          title='Avenged Grudges'
+          title="Avenged Grudges"
           grudges={avengedgrudges}
           onCheckOff={this.toggle}
           onRemove={this.removeGrudge}
         />
       </div>
-    )
+    );
   }
 }
 
-export default withAuthenticator(Application)
+export default Application;
